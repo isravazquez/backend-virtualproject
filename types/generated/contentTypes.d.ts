2f5e313456be116d64459138939e5abe00397e4a
isravazquez <isradronemx@gmail.com>
2023-10-17 17:24:57 -0600

    Hotfix in user model

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -764,7 +764,7 @@ export interface PluginUsersPermissionsUser extends Schema.CollectionType {
       'manyToOne',
       'plugin::users-permissions.role'
     >;
-    ContentAcces: Attribute.String;
+    contentAcces: Attribute.String;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     createdBy: Attribute.Relation<
