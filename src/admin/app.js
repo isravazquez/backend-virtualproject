3a78e4d105ff59604116d3c81e8afd6200a555b3
isravazquez <isradronemx@gmail.com>
2023-10-18 12:07:44 -0600

    Modify Login paga and main tittle

---
+++ src/admin/app.js
@@ -0,0 +1,46 @@
+const config = {
+  locales: [
+    // 'ar',
+    // 'fr',
+    // 'cs',
+    // 'de',
+    // 'dk',
+    // 'es',
+    // 'he',
+    // 'id',
+    // 'it',
+    // 'ja',
+    // 'ko',
+    // 'ms',
+    // 'nl',
+    // 'no',
+    // 'pl',
+    // 'pt-BR',
+    // 'pt',
+    // 'ru',
+    // 'sk',
+    // 'sv',
+    // 'th',
+    // 'tr',
+    // 'uk',
+    // 'vi',
+    // 'zh-Hans',
+    // 'zh',
+  ],
+  translations: {
+    en: {
+      'app.components.LeftMenu.navbrand.title': 'Virtual Project',
+      "Auth.form.welcome.title": "Bienvenido a Virtual Project",
+      "Auth.form.welcome.subtitle": "Inicia sesión",
+    },
+  },
+};
+
+const bootstrap = (app) => {
+  console.log(app);
+};
+
+export default {
+  config,
+  bootstrap,
+};
