095692b60ae46235592769f8347b4e53309624ca
isravazquez <isradronemx@gmail.com>
2023-10-19 12:14:58 -0600

    Increase limit upload file size, Change configutation on middlewares

--- config/middlewares.js
+++ config/middlewares.js
@@ -36,8 +36,8 @@ module.exports = [
     name: "strapi::body",
     config: {
       formLimit: "1024mb", // modify form body
-      jsonLimit: "1024", // modify JSON body
-      textLimit: "1024", // modify text body
+      jsonLimit: "1024mb", // modify JSON body
+      textLimit: "1024mb", // modify text body
       formidable: {
         maxFileSize: 1000 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
       },
